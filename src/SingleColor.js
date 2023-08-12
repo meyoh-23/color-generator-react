33e903d0c2aff18df67ee4649479fd12e25d870b
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-12 12:52:19 +0300

    timeout to clear alert

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -9,9 +9,15 @@ const SingleColor = ({rgb, weight, index}) => {
   const hexColor = rgbToHex(...rgb);
 
   return ( 
-    <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}>
+    <article className={`color ${index > 10 && 'color-light'}`}
+    style={{backgroundColor: `rgb(${bcg})`}}
+    onClick={()=>{
+      setAlert(true);
+      navigator.clipboard.writeText(hexColor)
+    }}>
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexColor}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   )
 }
